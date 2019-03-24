674be2e139d5c1b5fa72ae1b1241b12aed5b9e6b
Marco Antonio <marcoantonio@MacBook-Air-de-Marco.local>
2019-03-23 23:12:39 -0300

    ajustes no app-routing para trablhar as rotas, código intial para home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,4 +5,13 @@ import {HomeComponent} from '../app/home/home.component';
 
 const routes: Routes =[
     {path: '', component: HomeComponent },
+    {path: 'home', component: HomeComponent },
 ];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes, {useHash:true})],
+    exports: [RouterModule],
+    declarations:[]
+})
+
+export class AppRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +17,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     MenuComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     NgbModule
   ],
